ec4bc41ec7672e7c95015ea9349799bcc47c4b8a
Keiizen <94333291+Keiizen@users.noreply.github.com>
2024-03-02 14:31:22 +0000

    t

---
+++ js/elements.js
@@ -0,0 +1,38 @@
+function setupHTML() {
+	
+	
+	setupResourcesHTML()
+	
+	
+
+	
+
+	tmp.el = {}
+	let all = document.getElementsByTagName("*")
+	for (let i=0;i<all.length;i++) {
+		let x = all[i]
+		tmp.el[x.id] = new Element(x)
+	}
+
+    
+}
+
+
+
+
+function updateHTML() {
+	//document.documentElement.style.setProperty('--font', player.options.font)
+	//document.documentElement.style.setProperty('--cx', tmp.cx)
+	//document.documentElement.style.setProperty('--cy', tmp.cy)
+
+	tmp.mobile = window.innerWidth < 1200
+
+	let displayMainTab = true
+	
+
+    tmp.el.app.setDisplay(tmp.start && displayMainTab)
+	
+
+	updateResourcesHTML()
+	if (hover_tooltip) updateTooltipResHTML()
+}
---
+++ js/temp.js
@@ -0,0 +1,64 @@
+var tmp = {}
+
+function resetTemp() {
+    let d = new Date()
+    keep = [tmp.el, tmp.prevSave]
+    tmp = {
+        
+
+        cx: 0,
+        cy: 0,
+
+        mobile: false,
+
+        start: false,
+
+       
+        tab: 0,
+        stab: [],
+       
+        pass: 0,
+        notify: [],
+        popup: [],
+        saving: 0,
+       
+
+       
+
+       
+       
+
+        prevSave: "",
+
+       
+    }
+
+   
+    
+    tmp.el = keep[0]
+    tmp.prevSave = keep[1]
+    tmp.notify = {}
+   
+    tmp.pointGain = FORMS.pointGain()
+}
+
+function updateRagePowerTemp() {
+    if (!tmp.rp) tmp.rp = {}
+    tmp.rp.gain = FORMS.rp.gain()
+    tmp.rp.can = tmp.rp.gain.gte(1)
+}
+
+function updatePointTemp() {
+tmp.pointGain = FORMS.pointGain()
+}
+function updateGamespeedTemp() {
+    tmp.gs = FORMS.gameSpeed() 
+}
+function updateTemp() {
+    tmp.offlineActive = player.offline.time > 1
+    tmp.offlineMult = tmp.offlineActive?player.offline.time+1:1
+    updateGamespeedTemp()
+    updatePointTemp()
+   updateRagePowerTemp()
+   
+}
