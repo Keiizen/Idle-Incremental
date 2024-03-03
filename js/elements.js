e35b0b21d326aa9a11ba3fbc004397edba474067
Keiizen <94333291+Keiizen@users.noreply.github.com>
2024-03-03 22:58:53 +0000

    t

--- js/elements.js
+++ js/elements.js
@@ -8,13 +8,14 @@ function setupHTML() {
 	
 
 	let main_upgs_table = new Element("main_upgs_table")
+	table = ""
 	for (let x = 1; x <= UPGS.main.cols; x++) {
 		let id = UPGS.main.ids[x]
 		table += `<div id="main_upg_${x}_div" style="width: 230px; margin: 0px 10px;"><b>${UPGS.main[x].title}</b><br><br><div style="font-size: 13px; min-height: 50px" id="main_upg_${x}_res"></div><br><div class="table_center" style="justify-content: start;">`
 		for (let y = 1; y <= UPGS.main[x].lens; y++) {
 			let key = UPGS.main[x][y]
 			table += `<img onclick="UPGS.main[${x}].buy(${y})" onmouseover="UPGS.main.over(${x},${y})" onmouseleave="UPGS.main.reset()"
-			 style="margin: 3px;" class="img_btn" id="main_upg_${x}_${y}" src="${key.noImage?`images/test.png`:`images/upgrades/main_upg_${id+y}.png`}">`
+			 style="margin: 3px;" class="img_btn" id="main_upg_${x}_${y}" src="${`images/upgrades/main_upg_${id+y}.png`?`images/upgrades/main_upg_${id+y}.png`:`images/test.png`}">`
 		}
 		table += `</div><br><button id="main_upg_${x}_auto" class="btn" style="width: 80px;" onclick="player.auto_mainUpg.${id} = !player.auto_mainUpg.${id}">OFF</button></div>`
 	}
