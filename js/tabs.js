9657a11f1b91328a093de135ed13b9d98dfef0c1
Keiizen <94333291+Keiizen@users.noreply.github.com>
2024-03-03 10:01:55 +0000

    t

--- js/tabs.js
+++ js/tabs.js
@@ -1,21 +1,21 @@
 const TABS = {
     choose(id) {
-            /*let tab = id+"_tab"
+            let tab = id+"_tab"
             if (tab != player.curtab) {
                 player.curtab = tab
-            }*/
+            }
     },
-    //tabTable: {0: {Name: "main_tab"}, 1: {Name: "upgrades_tab"}}
+    tabTable: {0: {Name: "main_tab"}, 1: {Name: "upgrades_tab"}}
 }
 
 
 
 
 function updateTabs() {
-    /*for (i in TABS.tabTable) {
+    for (i in TABS.tabTable) {
         let tab = TABS.tabTable[i]
         tmp.el[tab.Name].setDisplay(tab.Name == player.curtab)
-    }*/
+    }
     
     tmp.el.openTabTerminal.setHTML(player.options.hidenav[0] ? "^" : "v")
     tmp.el.hideres.setHTML(player.options.hidenav[1] ? "^" : "v")
