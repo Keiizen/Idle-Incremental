b209accc799711d80ace96d329fa81ecb06a05f1
Keiizen <94333291+Keiizen@users.noreply.github.com>
2024-03-04 15:06:26 +0000

    txt

--- js/data.js
+++ js/data.js
@@ -153,6 +153,7 @@ function save(){
     if (localStorage.getItem("testSave") == '') wipe()
     localStorage.setItem("testSave",str)
     tmp.prevSave = localStorage.getItem("testSave")
+    if(tmp.saving<1){tmp.saving++;addNotify('Game saved.',3)}
 }
 
 function load(x){
