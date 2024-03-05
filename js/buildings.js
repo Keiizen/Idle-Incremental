528242c43405f5806ecd4690c6c787496d60b189
Keiizen <94333291+Keiizen@users.noreply.github.com>
2024-03-05 16:05:05 +0000

    t

--- js/buildings.js
+++ js/buildings.js
@@ -213,9 +213,9 @@ const BUILDINGS = {
     //DOM
 	setup() {
 		for (var [i, b] of Object.entries(BUILDINGS_DATA)) {
-            let el = new Element("building_"+i)
+            let e = new Element("building_"+i)
 
-			if (tmp.el.el) el.setHTML(`<div class="table_center upgrade" style="width: 100%; margin-bottom: 5px;">
+			if (tmp.el.e) e.setHTML(`<div class="table_center upgrade" style="width: 100%; margin-bottom: 5px;">
 				<div style="width: 300px">
 					<div class="resources">
 						<img src="images/buildings/${b.icon||"mark"}.png">
